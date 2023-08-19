e64aa94fc811fe09b0b3a0385d5db4f3df79edb8
Rabin karmakar <106877477+rabin97@users.noreply.github.com>
2023-08-19 19:30:14 +0530

    first commit

---
+++ js/client.js
@@ -0,0 +1,39 @@
+const socket = io("http://localhost:8000", { transports: ["polling"] });
+// const socket = io("https://send-message.onrender.com/", { transports: ['polling'] });
+const sendContainer = document.getElementById("send-container");
+const messageInput = document.getElementById("messageImp");
+const messageContainer = document.querySelector(".container");
+
+const append = (message, position) => {
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageElement.classList.add("message");
+  messageElement.classList.add(position);
+  messageContainer.appendChild(messageElement);
+};
+
+const names = prompt("Enter your name");
+
+socket.emit("new-user-joined", names);
+
+socket.on("user-joined", (data) => {
+  append(`${data} join the chat`, "right");
+});
+
+
+
+sendContainer.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const message = messageInput.value;
+  if (message.trim() !== "") {
+    socket.emit("send-message", message);
+    messageInput.value = "";
+  }
+});
+
+// Append messages with the appropriate position ("left" or "right")
+socket.on("receive-message", (data) => {
+  const position = data.sender === names ? "right" : "left";
+  append(`${data.message}`, position);
+});
+
